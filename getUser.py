961007e53294d002a258810cf281fff3bf44adbf
andreasma <maand@gmx.de>
2016-09-17 21:50:07 +0200

    First commit of a script to write user to a csv file, get a list of users and creators in two text files. The last two files could be used to evaluate users that has not contributed yet and clean up the account list.

---
+++ getUser.py
@@ -0,0 +1,29 @@
+from plone import api
+import csv
+
+portal = api.portal.get()
+
+users = api.user.get_users()
+
+# write all current users in append mode to a csv-file
+for f in users:
+    id = f.getProperty('id')
+    name = f.getProperty('fullname')
+    email = f.getProperty('email')
+    roles = api.user.get_roles(username=id)
+    with open('users.csv', 'ab') as csvfile:
+        memberlist = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        memberlist.writerow([name, id, email, roles])
+
+    with open('currentusers.txt', 'ab') as userlist:
+        userlist.write(f.id)
+        userlist.write('\n')
+
+catalog = api.portal.get_tool(name='portal_catalog')
+
+results = catalog(portal_type="tdf.templateuploadcenter.tupproject")
+
+for d in results:
+    with open('creators.txt', 'ab') as textfile:
+        textfile.write(d.Creator)
+        textfile.write('\n')
