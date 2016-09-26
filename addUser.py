e75d8d79debf93066449472f6936585f8f32b470
andreasma <maand@gmx.de>
2016-09-26 21:48:03 +0200

    Adding a feature to create a current users list after adding new users.

--- addUser.py
+++ addUser.py
@@ -4,6 +4,9 @@ import transaction
 
 csvfilename = raw_input("Please type in the name of the csv file with the data for the new user(s): ")
 
+outputcsvfile = raw_input("Please type the name of the output csv file, where the current user list with the"
+                          "added user should be written: ")
+
 portal = api.portal.get()
 
 # import new member from a new csv-file into Plone. The user provides
@@ -17,3 +20,17 @@ with open(csvfilename, 'rb') as f:
         api.user.create(username=row[2], email=row[1], properties ={'fullname':row[0]})
 
 transaction.commit()
+
+
+# get all current users from the Plone instance
+users = api.user.get_users()
+
+# write all current users in append mode to a csv file
+for f in users:
+  id = f.getProperty('id')
+  name = f.getProperty('fullname')
+  email = f.getProperty('email')
+  roles = api.user.get_roles(username = id)
+  with open(outputcsvfile, 'ab') as csvfile:
+    memberlist = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    memberlist.writerow([name,id,email,roles])
