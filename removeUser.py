1f9e7bb9a97f4fe3f51272797eb7c7c9a488a970
andreasma <maand@gmx.de>
2016-11-06 22:05:02 +0100

    first version of removeUser

---
+++ removeUser.py
@@ -0,0 +1,15 @@
+from plone import api
+import transaction
+
+
+listremoveusers = raw_input("Please type in the name of the text file with the usernames (only this names and"
+                            "only one name per line), which you want to delete and remove from the Plone"
+                            "instance: ")
+
+portal = api.portal.get()
+
+with open(listremoveusers, 'r') as f:
+    for line in f:
+        unwanteduser = f.line
+        api.user.delete(username=unwanteduser)
+    
