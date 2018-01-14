ad7dcedc39624fe4f2e04c1d9ed8492c0b229876
andreasma <maand@gmx.de>
2018-01-14 20:10:19 +0100

    add a print command to the remove script

--- removeListofusers.py
+++ removeListofusers.py
@@ -7,5 +7,6 @@ inputtextfile = raw_input("Please type in the name of the text file with the lis
 data = [line.strip() for line in open(inputtextfile, 'r')]
 
 for item in data:
+    print(item)
     api.user.delete(username=item)
     transaction.commit()
