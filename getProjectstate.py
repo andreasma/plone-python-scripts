2776721cdaa126b98934d10438715e15f16b7324
andreasma <maand@gmx.de>
2018-08-16 16:59:42 +0200

    Add a feature to get the name, state and url of all template projects from a Plone site and write them into a csv file.

--- getProjectstate.py
+++ getProjectstate.py
@@ -7,6 +7,8 @@ catalog = api.portal.get_tool(name='portal_catalog')
 
 allextprojects = catalog(portal_type='tdf.extensionuploadcenter.eupproject')
 
+alltempprojects = catalog(portal_type='tdf.templateuploadcenter.tupproject')
+
 for d in allextprojects:
      st = d.review_state
      url = ('/' .join (d.getObject().getPhysicalPath()))
@@ -15,3 +17,14 @@ for d in allextprojects:
      with open('extensionprojects.csv', 'ab') as et:
          pt = csv.writer(et, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
          pt.writerow([name, st, url])
+
+
+
+for t in alltempprojects:
+     st = t.review_state
+     url = ('/' .join (t.getObject().getPhysicalPath()))
+     name = t.Title
+
+     with open('templateprojects.csv', 'ab') as tup:
+         ptemp = csv.writer(tup, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+         ptemp.writerow([name, st, url])
